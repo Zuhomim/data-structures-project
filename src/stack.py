490d90d3c80d7d52587a70731ca0045d74d979f5
Dmitri Sementsov <pluribus.zuhi@gmail.com>
2023-10-02 23:54:40 +0700

    add stack, test_stack

--- src/stack.py
+++ src/stack.py
@@ -7,15 +7,18 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
     """Класс для стека"""
 
+    top = []
+
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
 
     def push(self, data):
         """
@@ -23,7 +26,10 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data, None)
+        if self.top:
+            new_node.next_node = self.top
+        self.top = new_node
 
     def pop(self):
         """
@@ -31,4 +37,8 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.top is None:
+            return None
+        result = self.top.data
+        self.top = self.top.next_node
+        return result
